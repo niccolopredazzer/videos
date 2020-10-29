3b3cc7b5b2368d276d3d471656b3a5e92b6bd8bd
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-10-29 19:55:28 +0100

    videoselect

--- src/components/App.js
+++ src/components/App.js
@@ -6,30 +6,37 @@ import VideoList from './VideoList';
 const KEY = 'AIzaSyDG5kDO9Hb7-mODvePIbLQyldjKi0mu2I4';
 
 class App extends React.Component {
-  state = { videos: [] };
-
-  onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term,
-        part: 'snippet',
-        maxResults: 5,
-        type: 'video',
-        key: KEY,
-      },
-    });
-
-    this.setState({ videos: response.data.items });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
-      </div>
-    );
+    state = { videos: [], selectedVideo: null };
+  
+    onTermSubmit = async (term) => {
+      const response = await youtube.get('/search', {
+        params: {
+          q: term,
+          part: 'snippet',
+          maxResults: 5,
+          type: 'video',
+          key: KEY,
+        },
+      });
+  
+      this.setState({ videos: response.data.items });
+    };
+  
+    onVideoSelect = (video) => {
+      //console.log('From the App!', video);
+    };
+  
+    render() {
+      return (
+        <div className="ui container">
+          <SearchBar onFormSubmit={this.onTermSubmit} />
+          <VideoList
+            onVideoSelect={this.onVideoSelect}
+            videos={this.state.videos}
+          />
+        </div>
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
